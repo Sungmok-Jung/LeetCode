686bbba0d8b1e2693563ec3c9db7deab20947ee6
Sungmok-Jung <126703817+Sungmok-Jung@users.noreply.github.com>
2025-10-02 18:44:45 +0900

    Time: 3719 ms (5%), Space: 13.4 MB (8.05%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -9,29 +9,29 @@ class Solution(object):
         
         i = 0 # start idx
         while i < len(s):
-            # print(i)
             result = ""
             dup = False # duplicate idx
             for j in range(i, len(s)):
-                # print(j)
-                # result.find(s[j])
                 
-                if (result.find(s[j]) == -1):
+                find = result.find(s[j])
+                
+                if (find == -1):
                     result += s[j]
                 else: # find!
-                    i += result.find(s[j]) +1
-                    # print(f"find! now, i is {result.find(s[j])+1}")
+                    i += find + 1
                     dup = True
                     break
                 
-            # print(f"result is {result}")
             lengths.append(len(result))
             if dup == False:
                 i = len(s)+1 # break from while loop
 
-
-        
         if len(lengths) > 0:
             return max(lengths)
         else:
             return 0
+
+test = Solution()
+s = "abcabcbb"
+answer = test.lengthOfLongestSubstring(s)
+print(answer)
